6ae5d540f7a05dfb1575a208397fabe4efb3ebec
rpbotforg-boop <rpbotforg@gmail.com>
2026-01-03 16:36:50 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,13 +3,13 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-OWNER = int(environ.get("OWNER", "7114926879"))
-CREDIT = "✿𝐒𝐔𝐉𝐀𝐋✿"
+API_ID = int(environ.get("API_ID", "22447622")
+API_HASH = environ.get("API_HASH", "543b62d58d3e723e766ba57a984ab65d")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8225118430:AAEOFTRcVH3Au1LR0iFyasUb4U5CGATuoT4")
+OWNER = int(environ.get("OWNER", "777756062"))
+CREDIT = "Ravindra"
 
-AUTH_USER = os.environ.get('AUTH_USERS', '7114926879').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '777756062').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
@@ -21,5 +21,6 @@ COLLECTION_NAME = environ.get("COLLECTION_NAME", "Telegrambot")
   
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
 
 
